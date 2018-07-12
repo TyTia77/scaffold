feaa72b05c4ffb24095a27634857bd7af52239b6
Ty Tia <tytia77@gmail.com>
2018-07-12 16:28:05 +1000

    added test

---
+++ generators/app/templates/_src/app/presentational/component/component.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import Component from './component'
+// import { shallowToJson } from 'enzyme-to-json'
+
+describe('<Component />', () => {
+    // const dom = shallow(<Component items={new Array(5).fill({})} />)
+
+    // it('knows that 2 and 2 make 4', () => {
+    //     expect(dom.instance().sum(2,2)).toBe(4)
+    // })
+
+    // it('handles numbers only', () => {
+    //     expect(dom.instance().sum('2',2)).toBe(null)
+    // })
+
+    // it('knows that 2 and 2 doesnt make 5', () => {
+    //     expect(dom.instance().sum(2,2)).not.toBe(5)
+    // })
+
+    // it('knows that 2 and 2 makes 0', () => {
+    //     expect(dom.instance().sub(2,2)).toBe(0)
+    // })
+
+    // it('handles numbers only', () => {
+    //     expect(dom.instance().sub([2], 2)).toBe(null)
+    //     expect(dom.instance().sub({abe: 1}, 2)).toBe(null)
+    // })
+
+    // it('is rendering 11 child components', () => {
+    //     expect(dom.find(Child).length).toBeLessThanOrEqual(11)
+    // })
+})
